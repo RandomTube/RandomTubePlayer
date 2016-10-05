026d2c011de9a033b1d581448258de402c9327c1
Alexander Krenz <failplayde@gmail.com>
2016-10-05 21:08:52 +0200

    Better PHP

--- randomtube.php
+++ randomtube.php
@@ -12,26 +12,20 @@ $video_array = array
 ); 
 
 //Page title
-$page_title = array
-('Your title');
+$title = 'Your title';
 
 //Page size. If you don't know what you're doing leave it like that. Normal settings are for a full, responsive page.
-$video_width = array
-('100%');
-$video_height = array
-('100%');
+$video_width = '100%';
+$video_height = '100%';
 
 //Player settings. Only change if you know what you're doing!
-$player_settings = array
-('?rel=0;amp;controls=0;showinfo=0;autoplay=1;modestbranding=0;iv_load_policy=3;cc_load_policy=1;disablekb=1&amp;enablejsapi=1&amp;loop=1&amp');
+$player = '?rel=0;amp;controls=0;showinfo=0;autoplay=1;modestbranding=0;iv_load_policy=3;cc_load_policy=1;disablekb=1&amp;enablejsapi=1&amp;loop=1&amp';
 
 //Stop editing. Now it should work :)
 shuffle($video_array);
 $video = $video_array[0]; 
-$width = $video_width[0];
-$height = $video_height[0];
-$title = $page_title[0];
-$player = $player_settings[0];
+$width = $video_width;
+$height = $video_height;
 ?>
 
 <title><?php echo $title; ?></title>
@@ -56,8 +50,5 @@ $player = $player_settings[0];
 <iframe id="bg" width="<?php echo $width; ?>" height="<?php echo $height; ?>" src="https://www.youtube.com/embed/<?php echo $video; ?><?php echo $player; ?>" frameborder="0" allowfullscreen="0"></iframe>
 </div>
 
-<!--
-<div id="content" style="display:inline"><h3 style="text-align: left" style="display:inline"><a href="https://youtube.com/watch?v=<?php echo $video; ?>">View on YouTube</a></h3></div> <div id="content" style="display:inline"><h3 style="text-align: right" style="display:inline"><a href="">Next Random Video</a></h3></div>
--->
 
 <div id="content" style="display:inline"><h3 style="text-align: left" style="display:inline"><a href="https://youtube.com/watch?v=<?php echo $video; ?>">View on YouTube</a></h3></div> <div style="display:inline" id="content"><h3 style="text-align: right" style="display:inline"><a href="">Next Random Video</a></h3></div>
