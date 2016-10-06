bbcd3104df83bf967c3d28f3682617f86848e3d2
Alexander Krenz <failplayde@gmail.com>
2016-10-06 21:49:15 +0200

    Switched to iFram API, thanks tooti312
    More customization soon...

--- randomtube.php
+++ randomtube.php
@@ -3,49 +3,84 @@
 //Page title
 $title = 'Your title';
 
-//Page size. If you don't know what you're doing leave it like that. Normal settings are for a full, responsive page.
-$video_width = '100%';
-$video_height = '100%';
-
-//Player settings. Only change if you know what you're doing!
-$player = '?rel=0;amp;controls=0;showinfo=0;autoplay=1;modestbranding=0;iv_load_policy=3;cc_load_policy=1;disablekb=1&amp;enablejsapi=1&amp;loop=1&amp';
-
+//Volume 100 = 100%, 10 = 10% etc
+$volume = "10";
 
 //Stop editing. Now it should work :)
 
+    $videos = file("videos.txt", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    shuffle($videos);
 
-$file = fopen("videos.txt", "r");
-$videos = array();
+    foreach($videos as $video)
 
-while (!feof($file))
-{
-   $videos[] = fgets($file);
-}
-
-fclose($file);
-var_dump($videos);
-
-shuffle($videos);
-$video = $videos[0]; 
-$width = $video_width;
-$height = $video_height;
 ?>
+
 
 <!DOCTYPE html>
 <html>
-   <head>
-      <title><?php echo $title; ?></title>
-      <meta name="keywords" content="video, random, whatever" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    	<link href="rtstyle.css" rel="stylesheet">
-   </head>
+    <head>
+        <title><?php echo $title; ?></title>
+        <meta name="keywords" content="video, random, whatever" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<style>
+	    	body, html
+		{
+		       margin: 0; padding: 0; height: 100%; overflow: hidden; background:#000;
+		}
 
+		#videocontainer
+		{
+			position: relative; width: 100%; height: 0; padding-bottom: 56.25%; z-index:1;
+		}
 
-   <div id="bg_container">
-      <iframe id="bg" width="<?php echo $width; ?>" height="<?php echo $height; ?>" src="https://www.youtube.com/embed/<?php echo $video; ?><?php echo $player; ?>" frameborder="0" allowfullscreen="0"></iframe>
-   </div>
+		#player 
+		{
+			position: absolute; top: 0; left: 0; width: 100%; height: 100%; z-index:1;
+		}
 
-
-   <div id="content" style="display:inline"><h3 style="text-align: left" style="display:inline"><a href="https://youtube.com/watch?v=<?php echo $video; ?>">View on YouTube</a></h3></div> <div style="display:inline" id="content"><h3 style="text-align: right" style="display:inline"><a href="">Next Random Video</a></h3></div>
+		#content
+		{
+                	z-index:2; position:relative; font-size: 1.17em; color: hotpink;
+		}
+            
+           	a:visited,a:link,a:hover
+		{
+			color: hotpink;
+		}
+	    </style>
+	    
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    </head>
+	
+    <body>
+        <div id="content">
+            <a href="https://youtube.com/watch?v=<?= $video; ?>" style="float:right">View on YouTube</a>
+            <a href=# onClick="window.location.reload()" >Next Random Video</a>        
+        </div>
+        
+    <div style="float: both;"></div>
+    
+    <div class="videocontainer">
+        <div id="player"></div>
+    </div>    
+    
+    <script>
+      var tag = document.createElement('script');
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+      var player;
+      function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+          videoId: '<?= $video ?>',
+          playerVars: { 'rel':0,'controls':1, 'showinfo':0, 'autoplay':1, 'modestbranding':0, 'iv_load_policy':3, 'cc_load_policy':1, 'disablekb':1,  'enablejsapi':0,  'loop':1 },
+          events: {'onReady': onPlayerReady}
+        });
+      }
+      function onPlayerReady(event) {
+        event.target.setVolume(<?= $volume ?>);
+        event.target.playVideo();
+      }
+    </script>
 
 </html>
