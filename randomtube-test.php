f9cdb293ae2278e17cf0c468483fe1d5dfc2ccb1
Alexander Krenz <failplayde@gmail.com>
2016-10-06 21:42:45 +0200

    Update randomtube-test.php

--- randomtube-test.php
+++ randomtube-test.php
@@ -1,10 +1,20 @@
 <?php 
-// Help: https://developers.google.com/youtube/iframe_api_reference?hl=de
-
-
 //Page title
-$title = 'Random shit!';
+$title = 'Random shit';
+
+//Volume 100 = 100%, 10 = 10% etc
+$volume = "10";
+
+//Stop editing. Now it should work :)
+
+    $videos = file("videos.txt", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    shuffle($videos);
+
+    foreach($videos as $video)
+        echo "new featuredProduct('', '". urlencode(trim($line)) ."'),\n<br />";
+
 ?>
+
 
 <!DOCTYPE html>
 <html>
@@ -12,68 +22,65 @@ $title = 'Random shit!';
         <title><?php echo $title; ?></title>
         <meta name="keywords" content="video, random, whatever" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link href="rtstyle.css" rel="stylesheet">
+	<style>
+	    	body, html
+		{
+		       margin: 0; padding: 0; height: 100%; overflow: hidden; background:#000;
+		}
+
+		#videocontainer
+		{
+			position: relative; width: 100%; height: 0; padding-bottom: 56.25%; z-index:1;
+		}
+
+		#player 
+		{
+			position: absolute; top: 0; left: 0; width: 100%; height: 100%; z-index:1;
+		}
+
+		#content
+		{
+                	z-index:2; position:relative; font-size: 1.17em; color: hotpink;
+		}
+            
+           	a:visited,a:link,a:hover
+		{
+			color: hotpink;
+		}
+	    </style>
+	    
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     </head>
 	
-	
-  <body>
-    <!-- 1. The <iframe> (and video player) will replace this <div> tag. -->
-
-<div style="display:inline" id="content"><h3 style="text-align: right" style="display:inline"><a href="">Next Random Video</a> | <a id="pause-button" href="#"><strong>Pause The Video</strong></a>| <a id="play-button" href="#"><strong>Play The Video</strong></a></h3></div>
-
-	  
-    <div id="player"></div>
-
+    <body>
+        <div id="content">
+            <a href="https://youtube.com/watch?v=<?= $video; ?>" style="float:right">View on YouTube</a>
+            <a href=# onClick="window.location.reload()" >Next Random Video</a>        
+        </div>
+        
+    <div style="float: both;"></div>
+    
+    <div class="videocontainer">
+        <div id="player"></div>
+    </div>    
+    
     <script>
-      // 2. This code loads the IFrame Player API code asynchronously.
       var tag = document.createElement('script');
-
       tag.src = "https://www.youtube.com/iframe_api";
       var firstScriptTag = document.getElementsByTagName('script')[0];
       firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-      // 3. This function creates an <iframe> (and YouTube player)
-      //    after the API code downloads.
       var player;
       function onYouTubeIframeAPIReady() {
         player = new YT.Player('player', {
-          height: '100%',
-          width: '100%',
-          videoId: 'M7lc1UVf-VE',
-          events: {
-            'onReady': onPlayerReady,
-          }
+          videoId: '<?= $video ?>',
+          playerVars: { 'rel':0,'controls':1, 'showinfo':0, 'autoplay':1, 'modestbranding':0, 'iv_load_policy':3, 'cc_load_policy':1, 'disablekb':1,  'enablejsapi':0,  'loop':1 },
+          events: {'onReady': onPlayerReady}
         });
       }
-
-      // 4. The API will call this function when the video player is ready.
       function onPlayerReady(event) {
+        event.target.setVolume(<?= $volume ?>);
         event.target.playVideo();
       }
+    </script>
 
-		
-function onPlayerReady(event) {
-  
-  // bind events
-  var playButton = document.getElementById("play-button");
-  playButton.addEventListener("click", function() {
-    player.playVideo();
-  });
-  
-  var pauseButton = document.getElementById("pause-button");
-  pauseButton.addEventListener("click", function() {
-    player.pauseVideo();
-  });
-  
-}
-	  </script>
-	  
-	  
-
-
-	  
-	  
-
-
-  </body>
+</html>
