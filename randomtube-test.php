7672b6103264a2cfaaaf3d055f55c7e2006808e7
Alexander Krenz <failplayde@gmail.com>
2016-10-08 00:50:05 +0200

    Update randomtube-test.php

--- randomtube-test.php
+++ randomtube-test.php
@@ -2,17 +2,14 @@
 //How this looks like: http://mysl.it/randomtube/indev/000.php
 
 //Page title
-$title = 'Random shit';
+$title = "Random shit";
 
 //Volume 100 = 100%, 10 = 10% etc
 $volume = "10";
 
 //Stop editing. Now it should work :)
 
-    $videos = file("videos.txt", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-    shuffle($videos);
-
-    foreach($videos as $video)
+$videos = file("videos.txt", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
 ?>
 
@@ -24,34 +21,13 @@ $volume = "10";
         <meta name="keywords" content="video, random, whatever" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<style>
-	    body, html {
-		       margin: 0; padding: 0; height: 100%; overflow: hidden; background:#000;
-		}
-
-		#videocontainer {
-			position: relative; width: 100%; height: 0; padding-bottom: 56.25%; z-index:1;
-		}
-
-		#player {
-			position: absolute; top: 0; left: 0; width: 100%; height: 100%; z-index:1;
-		}
-
-		#contentbkp {
-                	z-index:2; position:relative; font-size: 1.17em; color: hotpink;
-		}
-            
-		#content {
-                	z-index:2; position:fixed; bottom:8px; text-align:center; font-size: 1.17em; color: hotpink; 
-		}
-		
-		a:visited,a:link,a:hover {
-			color: hotpink;
-		}
-		
-			
-	    </style>
-	    
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	    body, html { margin: 0; padding: 0; height: 100%; overflow: hidden; background:#000; }
+		#videocontainer { position: relative; width: 100%; height: 0; padding-bottom: 56.25%; z-index:1; }
+		#player { position: absolute; top: 0; left: 0; width: 100%; height: 100%; z-index:1; }
+		#contentbkp { z-index:2; position:relative; font-size: 1.17em; color: hotpink; }
+		#content { z-index:2; position:fixed; bottom:8px; text-align:center; font-size: 1.17em; color: hotpink; }
+		a:visited,a:link,a:hover { color: hotpink; }
+	</style>
     </head>
 	
     <body>
@@ -59,7 +35,7 @@ $volume = "10";
         <div id="content" style="float:center">
 			<strong>
 			<a href=# onClick="window.location.reload()" >Next Random Video</a>
-            <a href="https://youtube.com/watch?v=<?= $video; ?>">View on YouTube</a>
+            <a href="https://youtube.com/watch?v=<?= $videos[rand(0, count($videos) -1)] ?>" target="_blank">Watch on YouTube</a>
 			<strong>
         </div>
 
@@ -78,7 +54,7 @@ $volume = "10";
       var player;
       function onYouTubeIframeAPIReady() {
         player = new YT.Player('player', {
-          videoId: '<?= $video ?>',
+          videoId: '<?= $videos[rand(0, count($videos) -1)] ?>',
           playerVars: { 'rel':0,'controls':1, 'showinfo':0, 'autoplay':1, 'modestbranding':0, 'iv_load_policy':3, 'cc_load_policy':1, 'disablekb':1,  'enablejsapi':0,  'loop':1 },
           events: {'onReady': onPlayerReady}
         });
